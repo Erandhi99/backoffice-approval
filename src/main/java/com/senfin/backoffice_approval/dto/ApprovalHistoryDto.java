17b65e790cffd6ac51450411ddc2f9c6976f46be
Erandhi99 <madhushikabae.20@uom.lk>
2026-09-03 08:19:17 +0530

    Change approval history dto

--- src/main/java/com/senfin/backoffice_approval/dto/ApprovalHistoryDto.java
+++ src/main/java/com/senfin/backoffice_approval/dto/ApprovalHistoryDto.java
@@ -1,13 +1,13 @@
 package com.senfin.backoffice_approval.dto;
 
-// import com.senfin.backoffice_approval.entity.ApprovalStage;
-// import com.senfin.backoffice_approval.entity.HistoryAction;
+import com.senfin.backoffice_approval.entity.ApprovalStage;
+import com.senfin.backoffice_approval.entity.HistoryAction;
 
 import java.time.Instant;
 
 public record ApprovalHistoryDto(
-        // HistoryAction action,
-        // ApprovalStage stage,
+        HistoryAction action,
+        ApprovalStage stage,
         String performedByUsername,
         String performedByRole,
         String comment,
